941c51493aa9dc82f6a1a330c7323d61fab1d451
Tatiane Mossi <mossitatiane2@gmail.com>
2022-08-25 17:21:58 -0300

    Implementando painel principal

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthenticationService } from '../auth/services/authentication.service';
 
 @Component({
   selector: 'app-painel',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./painel.component.css']
 })
 export class PainelComponent implements OnInit {
+  emailUsuario?: string | null;
 
-  constructor() { }
+  constructor(
+    private authService: AuthenticationService,
+    private router: Router) { }
 
   ngOnInit(): void {
+    this.authService.usuarioLogado
+      .subscribe(usuario => this.emailUsuario = usuario?.email)
   }
 
+  public sair() {
+    this.authService.logout()
+      .then(() => this.router.navigate(['/login']))
+  }
 }
