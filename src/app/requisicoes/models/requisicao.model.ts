f57e18d6d14b25b1fdf2206a446c7bd90dea2f81
Tatiane Mossi <mossitatiane2@gmail.com>
2022-09-06 16:35:51 -0300

    Adicionando funcionário no model da requisição.

--- src/app/requisicoes/models/requisicao.model.ts
+++ src/app/requisicoes/models/requisicao.model.ts
@@ -1,5 +1,6 @@
 import { Departamento } from "src/app/departamentos/models/departamento.model";
 import { Equipamento } from "src/app/equipamentos/models/equipamento.model";
+import { Funcionario } from "src/app/funcionarios/models/funcionario.model";
 
 export class Requisicao {
   id: string;
@@ -9,4 +10,6 @@ export class Requisicao {
   descricao: string;
   equipamentoId?: string;
   equipamento?: Equipamento;
+  funcionario: Funcionario;
+  funcionarioId: string;
 }
